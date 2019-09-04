33e76972cfbd525f2b004ccff5e7c7dc8d2a99b9
Bruno Pastorelli <brunopastorelli@hotmail.com>
2019-09-04 19:52:53 -0300

    Primeiro commit

---
+++ src/main/java/com/associacao/arrecadacao/api/ArrecadacaoAssociacaoApplication.java
@@ -0,0 +1,13 @@
+package com.associacao.arrecadacao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrecadacaoAssociacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrecadacaoAssociacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/associacao/arrecadacao/api/ArrecadacaoAssociacaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.associacao.arrecadacao.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArrecadacaoAssociacaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
