a4e6df5b9a2a227f42dd0717bf8bba3b5b9ade15
Bruno Pastorelli <brunopastorelli@hotmail.com>
2019-09-05 16:56:25 -0300

    Criado serviço Residencia

---
+++ src/main/java/com/associacao/arrecadacao/api/services/ResidenciaService.java
@@ -0,0 +1,25 @@
+package com.associacao.arrecadacao.api.services;
+
+import java.util.Optional;
+
+import com.associacao.arrecadacao.api.entities.Residencia;
+
+public interface ResidenciaService {
+	
+	/**
+	 * Retorna uma residência dado uma matricula.
+	 * 
+	 * @param matricula
+	 * @return Optional<Residencia>
+	 */
+	Optional<Residencia> bucarPorMatricula(String matricula);
+	
+	/**
+	 * Cadastra uma nova residencia na base de dados.
+	 * 
+	 * @param residencia
+	 * @return Residencia
+	 */
+	Residencia persistir(Residencia residencia);
+
+}
---
+++ src/main/java/com/associacao/arrecadacao/api/services/impl/ResidenciaServiceImpl.java
@@ -0,0 +1,33 @@
+package com.associacao.arrecadacao.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.associacao.arrecadacao.api.entities.Residencia;
+import com.associacao.arrecadacao.api.repositories.ResidenciaRepository;
+import com.associacao.arrecadacao.api.services.ResidenciaService;
+
+@Service
+public class ResidenciaServiceImpl implements ResidenciaService {
+	
+	private static final Logger log = LoggerFactory.getLogger(ResidenciaServiceImpl.class);
+
+	@Autowired
+	private ResidenciaRepository residenciaRepository;
+	
+	@Override
+	public Optional<Residencia> bucarPorMatricula(String matricula) {
+		log.info("Buscando uma residência para a matricula {}", matricula);
+		return Optional.ofNullable(residenciaRepository.findByMatricula(matricula));
+	}
+
+	@Override
+	public Residencia persistir(Residencia residencia) {
+		log.info("Persistindo a residência: {}", residencia);
+		return this.residenciaRepository.save(residencia);
+	}
+}
