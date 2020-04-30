c7a932e83e39c0830854e2d5900992a36e3e151d
bpastorelli <papitopastorelli@gmail.com>
2020-04-30 00:42:11 -0300

    Implementado cache nas funcionalidades de lançamentos

--- src/main/java/com/associacao/arrecadacao/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/associacao/arrecadacao/api/services/impl/LancamentoServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -22,13 +24,13 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
-	@Override
+	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscar lançamento por ID {}", id);
 		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
 	}
 
-	@Override
+	@CachePut("lancamentoPorId")
 	public List<Lancamento> persistir(List<Lancamento> lancamentos) {
 		log.info("Persistindo o lançamento {}", lancamentos);
 		return this.lancamentoRepository.save(lancamentos);
@@ -52,7 +54,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 		return this.lancamentoRepository.findByResidenciaId(residenciaId);
 	}
 
-	@Override
+	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorPeriodoAndResidenciaId(String periodo, Long residenciaId) {
 		log.info("Buscar lançamento por Periodo {}", periodo, residenciaId);
 		return this.lancamentoRepository.findByPeriodoAndResidenciaId(periodo, residenciaId);
