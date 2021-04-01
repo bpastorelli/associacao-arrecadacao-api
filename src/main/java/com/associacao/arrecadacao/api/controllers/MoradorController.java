7e30777efea7f2fc01c6aecb3bedf5df5219bbdc
bpastorelli <brunopastorelli@hotmail.com>
2021-03-31 23:23:42 -0300

    Validação de registro existente

--- src/main/java/com/associacao/arrecadacao/api/controllers/MoradorController.java
+++ src/main/java/com/associacao/arrecadacao/api/controllers/MoradorController.java
@@ -97,7 +97,12 @@ public class MoradorController {
 		CadastroMoradorDto dto = new CadastroMoradorDto();
 		
 		List<Morador> listMorador = new ArrayList<Morador>();
+		List<Morador> listMoradorIncluir = new ArrayList<Morador>();
+		
 		listMorador.add(morador);
+		//Recebe os itens da listMorador
+		listMoradorIncluir.addAll(listMorador);
+		
 		dto.setMoradores(listMorador);
 		
 		validarDadosExistentes(dto, result);
@@ -106,9 +111,27 @@ public class MoradorController {
 			log.error("Erro validando dados para cadastro do(s) morador(es): {}", result.getAllErrors());
 			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
 			return ResponseEntity.badRequest().body(response.getErrors());
-		}	
+		}
 		
-		this.moradorService.persistir(listMorador);
+		//Valida se o CPF já existe, se existir sai da lista de inclusão
+		listMoradorIncluir.forEach(m -> {
+			this.moradorService.buscarPorCpf(m.getCpf())
+				.ifPresent(p -> listMoradorIncluir.removeIf(x -> x.getCpf() == p.getCpf()));
+		});
+		
+		//Insere os itens
+		this.moradorService.persistir(listMoradorIncluir);
+		
+		//Vincula o morador
+		listMorador.forEach(m -> {			
+			if(m.getResidenciaId() != null) {
+				this.vinculoResidenciaService.buscarPorResidenciaIdAndMoradorId(m.getResidenciaId().get(), morador.getId())
+					.ifPresent(p -> listMorador.removeIf(x -> x.getCpf() == p.getMorador().getCpf()));
+			}		
+		});
+		
+		this.vinculoResidenciaService.persistir(preencheVinculo(listMorador));
+		
 		response.setData(morador);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 		
@@ -295,6 +318,25 @@ public class MoradorController {
 		}
 		
 		return new ResponseEntity<>(moradores.getContent(), HttpStatus.OK);
+	}
+	
+	//Prepara o objeto de vinculo de residencia
+	private List<VinculoResidencia> preencheVinculo(List<Morador> listMoradores) {
+		
+		Residencia residencia = new Residencia();
+		List<VinculoResidencia> listVinculo = new ArrayList<VinculoResidencia>();		
+		
+		listMoradores.forEach(m -> {
+			VinculoResidencia vinculo = new VinculoResidencia();
+			residencia.setId(m.getResidenciaId().get());
+			vinculo.setMorador(m);
+			vinculo.setResidencia(residencia);
+			
+			listVinculo.add(vinculo);			
+			
+		});
+		
+		return listVinculo;
 	}
 	
 	private void validarDadosExistentes(CadastroMoradorDto cadastroMoradorDto, BindingResult result) {
