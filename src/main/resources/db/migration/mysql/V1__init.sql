90b866863b45dc0191083174d0af3cb67ecdfeb8
bpastorelli <brunopastorelli@hotmail.com>
2021-04-04 00:42:01 -0300

    Corrigida query

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -25,7 +25,7 @@ CREATE TABLE `morador` (
   `data_atualizacao` datetime NOT NULL,
   `data_criacao` datetime NOT NULL,
   `posicao` int(1) NOT NULL,
-  `associado` int(1) NOT NULL,
+  `associado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `visitante` (
