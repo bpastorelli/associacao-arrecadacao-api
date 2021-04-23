c492c6b3818c1ada09f62692c8177d251bf2c650
bpastorelli <brunopastorelli@hotmail.com>
2021-04-23 12:13:38 -0300

    Criado repository de modulo

---
+++ src/main/java/com/associacao/arrecadacao/api/access/repositories/ModuloRepository.java
@@ -0,0 +1,23 @@
+package com.associacao.arrecadacao.api.access.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.associacao.arrecadacao.api.access.entities.Modulo;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ModuloRepository extends JpaRepository<Modulo, Long> {
+	
+	Optional<Modulo> findById(Long id);
+	
+	Optional<Modulo> findByDescricao(String descricao);
+	
+	Page<Modulo> findAll(Pageable pageable);
+
+}
